a5c06eeb3dc62196e17ea9fd89f2df3b96b4d777
Abdur Rehman <wabdulrehman@yahoo.com>
2021-08-24 17:50:08 +0100

    Error in last commit: Deposit feature instead of withdrawl

--- src/test/java/TransactionTest.java
+++ src/test/java/TransactionTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TransactionTest {
     @Test
-    void createsWithdrawalTransaction(){
+    void createsDepositTransaction(){
         Transaction subject = new Transaction(50, 0, LocalDate.of(19, 2, 19));
         assertEquals(50, subject.getCredit());
         assertEquals(LocalDate.of(19,2,19), subject.getDate());
