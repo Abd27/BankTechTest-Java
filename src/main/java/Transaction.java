980f4693c879f8eb42a55f707253ff837204597a
Abdur Rehman <wabdulrehman@yahoo.com>
2021-08-24 17:55:19 +0100

    Transaction for withdrawl

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,14 +1,20 @@
 import java.time.LocalDate;
 public class Transaction {
     private Integer credit;
+    private Integer debit;
     private LocalDate date;
     public Transaction(Integer credit, Integer debit, LocalDate date){
         this.credit = credit;
+        this.debit = debit;
         this.date = date;
     }
 
     public Integer getCredit(){
         return this.credit;
+    }
+
+    public Integer getDebit(){
+        return this.debit;
     }
 
     public LocalDate getDate(){
--- src/test/java/TransactionTest.java
+++ src/test/java/TransactionTest.java
@@ -1,7 +1,10 @@
 import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class TransactionTest {
     @Test
@@ -9,5 +12,13 @@ public class TransactionTest {
         Transaction subject = new Transaction(50, 0, LocalDate.of(19, 2, 19));
         assertEquals(50, subject.getCredit());
         assertEquals(LocalDate.of(19,2,19), subject.getDate());
+    }
+
+    @Test
+    void createWithdrawlTransaction(){
+        Transaction subject = new Transaction(0, 50, LocalDate.of(19, 2, 19));
+        assertEquals(50, subject.getDebit());
+        assertEquals(LocalDate.of(19,2,19), subject.getDate());
+        assertEquals(0, subject.getCredit());
     }
 }
